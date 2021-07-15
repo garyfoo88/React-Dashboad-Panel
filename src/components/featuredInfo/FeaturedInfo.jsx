f8a99cf1138a20ad1bca2e4d59b6379b6db68f74
garyfoo88 <reid9000200020002000@gmail.com>
2021-07-16 00:50:26 +0800

    chart setup

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -7,9 +7,9 @@ function FeaturedInfo() {
       <div className="featured-item">
         <span className="featured-title">Revenue</span>
         <div className="featured-money-container">
-          <span className="feature-money">$6,432</span>
-          <span className="feature-money-rate">
-            -1.2 <ArrowDownward />
+          <span className="featured-money">$6,432</span>
+          <span className="featured-money-rate">
+            -1.2 <ArrowDownward className="featured-icon negative"/>
           </span>
         </div>
         <span className="featured-sub">Compared to last month</span>
@@ -17,9 +17,9 @@ function FeaturedInfo() {
       <div className="featured-item">
         <span className="featured-title">Sales</span>
         <div className="featured-money-container">
-          <span className="feature-money">$2,000</span>
-          <span className="feature-money-rate">
-            -1.2 <ArrowDownward />
+          <span className="featured-money">$2,000</span>
+          <span className="featured-money-rate">
+            -1.2 <ArrowDownward className="featured-icon negative"/>
           </span>
         </div>
         <span className="featured-sub">Compared to last month</span>
@@ -27,9 +27,9 @@ function FeaturedInfo() {
       <div className="featured-item">
         <span className="featured-title">Cost</span>
         <div className="featured-money-container">
-          <span className="feature-money">$2,000</span>
-          <span className="feature-money-rate">
-            11.2 <ArrowUpward />
+          <span className="featured-money">$2,000</span>
+          <span className="featured-money-rate">
+            11.2 <ArrowUpward className="featured-icon" />
           </span>
         </div>
         <span className="featured-sub">Compared to last month</span>
