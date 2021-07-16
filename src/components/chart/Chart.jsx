574b0f2f7c65c3fb89aba7eb81c5b7f6a1e458a2
garyfoo88 <reid9000200020002000@gmail.com>
2021-07-16 20:34:32 +0800

    React charts

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import "./chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { dummyData } from "../../utils/mockData";
 function Chart() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="chart">
+      <h3 className="chart-title">Sales Analytics</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+          <LineChart data={dummyData}>
+              <XAxis dataKey="name" stroke="#5550bd" />
+              <YAxis dataKey="Active_User" stroke="#5550bd" />
+              <Line type="monotone" dataKey="Active_User" stroke="#5550bd"/>
+              <Tooltip />
+              <Legend />
+              <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+          </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
-export default Chart
+export default Chart;
---
+++ src/utils/mockData.js
@@ -0,0 +1,74 @@
+export const dummyData = [
+  {
+    name: "Jan",
+    Active_User: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    Active_User: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    Active_User: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Apr",
+    Active_User: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "May",
+    Active_User: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Jun",
+    Active_User: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Jul",
+    Active_User: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Jul",
+    Active_User: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Aug",
+    Active_User: 5490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Sep",
+    Active_User: 1490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Nov",
+    Active_User: 5490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Dec",
+    Active_User: 6490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
